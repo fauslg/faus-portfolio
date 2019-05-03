aa5901f9eae3e47c7f8a9d91e450e4757c7236fd
Faustino Gaitan <faustino@thirdlove.com>
2019-05-02 21:29:13 -0300

    Updates

---
+++ pages-manifest-eee55044c9808e523ff2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{143:function(a){a.exports={pages:[{componentChunkName:"component---src-pages-index-js",jsonName:"index",path:"/"},{componentChunkName:"component---src-pages-about-js",jsonName:"about-f34",path:"/about/"}],dataPaths:{"about-f34":"708/path---about-f-34-4c2-0SUcWyAf8ecbYDsMhQkEfPzV8","dev-404-page-5f9":"386/path---dev-404-page-5-f-9-fab-0SUcWyAf8ecbYDsMhQkEfPzV8",index:"140/path---index-6a9-0SUcWyAf8ecbYDsMhQkEfPzV8"}}}}]);
+//# sourceMappingURL=pages-manifest-eee55044c9808e523ff2.js.map
